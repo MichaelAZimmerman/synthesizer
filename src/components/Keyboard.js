f1af963bce0b2147b7ef229170638dcabe4a7d62
Michael Zimmerman <mikezne@gmail.com>
2021-07-08 11:38:58 -0500

    adds tremelo, crusher, dist, and delay. hard coded

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import * as Tone from "tone";
 import playNote from "../shared/playNote";
 
 export default function Keyboard() {
--- src/shared/playNote.js
+++ src/shared/playNote.js
@@ -2,8 +2,26 @@ import React from "react";
 import * as Tone from "tone";
 
 export default function playNote(note) {
-  const synth = new Tone.Synth().toDestination();
-  synth.oscillator.type = "sawtooth";
-
-  synth.triggerAttackRelease(`${note}4`, "8n");
+  const bitCrush = 16;
+  const distAmt = 1;
+  const tremAmt = 0;
+  const pingPongRate = "4n";
+  const pingPongDepth = 0;
+  const pingPong = new Tone.PingPongDelay(
+    pingPongRate,
+    pingPongDepth
+  ).toDestination();
+  const tremolo = new Tone.Tremolo(tremAmt, 1).toDestination().start();
+  const crusher = new Tone.BitCrusher(bitCrush).toDestination();
+  const dist = new Tone.Distortion(distAmt).toDestination();
+  const synth = new Tone.Synth()
+    .connect(dist)
+    .connect(crusher)
+    .connect(tremolo)
+    .connect(pingPong);
+  const oscType = "triangle";
+  const octave = "4";
+  const noteType = "8n";
+  synth.oscillator.type = oscType;
+  synth.triggerAttackRelease(`${note}${octave}`, noteType);
 }
