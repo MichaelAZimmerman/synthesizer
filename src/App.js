d2593431507251cbffef73ec01346b628e74b388
Michael Zimmerman <mikezne@gmail.com>
2021-07-09 15:03:36 -0500

    adds login page with minimal styling

--- src/App.js
+++ src/App.js
@@ -17,56 +17,71 @@ function App() {
   const { username, logout } = useContext(UserContext);
   return (
     <Router>
-      <>
-        <header>
-          {username && <h2 className="text-center">Welcome {username}</h2>}
-        </header>
-        <nav className="flex-wrap">
-          {!username && (
-            <>
-              <NavLink activeClassName="" className="" to="/login">
-                Login
-              </NavLink>
-              <NavLink activeClassName="" className="" to="/about">
-                About
-              </NavLink>
-            </>
-          )}
-          {username && (
-            <>
-              <NavLink activeClassName="" className="" to="/keyboard">
-                Keyboard
-              </NavLink>
-              <NavLink
-                className="text-center"
-                to="/login"
-                onClick={() => {
-                  logout();
-                }}
-              >
-                Logout
-              </NavLink>
-            </>
-          )}
-        </nav>
-        <main>
-          <Switch>
-            {/* <Route path="/about"><About /></Route> */}
-            <ProtectedRoute path="/login" reqUser={false}>
-              <Login />
-            </ProtectedRoute>
-            <ProtectedRoute path="/keyboard" reqUser={true}>
-              <KeyBoard />
-            </ProtectedRoute>
-            <ProtectedRoute path="/about" reqUser={false}>
-              <About />
-            </ProtectedRoute>
-            <Route path="*">
-              <Redirect to="/login" />
-            </Route>
-          </Switch>
-        </main>
-      </>
+      <header>
+        {username ? (
+          <h2 className="text-center">Welcome {username}</h2>
+        ) : (
+          <h2 className="text-center">Please log in to access keyboard.</h2>
+        )}
+      </header>
+      <nav className="flex-wrap">
+        {!username && (
+          <>
+            <NavLink
+              activeClassName=""
+              className="link text-center"
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              activeClassName=""
+              className="link text-center"
+              to="/about"
+            >
+              About
+            </NavLink>
+          </>
+        )}
+        {username && (
+          <>
+            <NavLink
+              activeClassName="active"
+              className="link text-center"
+              to="/keyboard"
+            >
+              Keyboard
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              className="text-center"
+              to="/login"
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logout
+            </NavLink>
+          </>
+        )}
+      </nav>
+      <main className="text-center">
+        <Switch>
+          {/* <Route path="/about"><About /></Route> */}
+          <ProtectedRoute path="/login" reqUser={false}>
+            <Login />
+          </ProtectedRoute>
+          <ProtectedRoute path="/keyboard" reqUser={true}>
+            <KeyBoard />
+          </ProtectedRoute>
+          <ProtectedRoute path="/about" reqUser={false}>
+            <About />
+          </ProtectedRoute>
+          <Route path="*">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </main>
     </Router>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,47 @@
 import React, { useState, useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Login = () => {
-  return <></>;
+  const { login } = useContext(UserContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <>
+      <h2>Login</h2>
+      <form className="form">
+        <div className="form-field flex-wrap">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </div>
+        <div className="form-field flex-wrap">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <button
+          className="btn"
+          onClick={(e) => {
+            e.preventDefault();
+            if (username.length > 4 && password.length > 4) {
+              login(username);
+              // history.push("/search");
+            }
+          }}
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
