b63401731007fd136707ae78c5e15f270a4afd6c
Michael Zimmerman <mikezne@gmail.com>
2021-07-09 14:25:11 -0500

    sets up routing/protected routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
 import KeyBoard from "./components/Keyboard";
+import Login from "./components/Login";
+import About from "./components/About";
+import { UserContext } from "./context/UserContext";
+import ProtectedRoute from "./shared/ProtectedRoute";
 
 function App() {
+  const { username, logout } = useContext(UserContext);
   return (
-    <div>
-      <KeyBoard />
-    </div>
+    <Router>
+      <>
+        <header>
+          {username && <h2 className="text-center">Welcome {username}</h2>}
+        </header>
+        <nav className="flex-wrap">
+          {!username && (
+            <>
+              <NavLink activeClassName="" className="" to="/login">
+                Login
+              </NavLink>
+              <NavLink activeClassName="" className="" to="/about">
+                About
+              </NavLink>
+            </>
+          )}
+          {username && (
+            <>
+              <NavLink activeClassName="" className="" to="/keyboard">
+                Keyboard
+              </NavLink>
+              <NavLink
+                className="text-center"
+                to="/login"
+                onClick={() => {
+                  logout();
+                }}
+              >
+                Logout
+              </NavLink>
+            </>
+          )}
+        </nav>
+        <main>
+          <Switch>
+            {/* <Route path="/about"><About /></Route> */}
+            <ProtectedRoute path="/login" reqUser={false}>
+              <Login />
+            </ProtectedRoute>
+            <ProtectedRoute path="/keyboard" reqUser={true}>
+              <KeyBoard />
+            </ProtectedRoute>
+            <ProtectedRoute path="/about" reqUser={false}>
+              <About />
+            </ProtectedRoute>
+            <Route path="*">
+              <Redirect to="/login" />
+            </Route>
+          </Switch>
+        </main>
+      </>
+    </Router>
   );
 }
 
