b050ee204b9287041967ad64826bd0b327353469
Michael Zimmerman <mikezne@gmail.com>
2021-07-11 23:48:07 -0500

    adds text to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,11 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <h2>This is an about page.</h2>
+      <h4 className="about">
+        This page is for messing around with synthesizer effects and settings
+        using Tone.js. Click "Keyboard" in the menu to begin. It seems to glitch
+        out after playing roughly 40 notes.
+      </h4>
     </>
   );
 };
