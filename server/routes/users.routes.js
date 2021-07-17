176deb06791063bb05e5e99a5303c0055b260316
Michael Zimmerman <mikezne@gmail.com>
2021-07-17 13:25:21 -0500

    adds signup routing inusers.routes.js

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,3 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { signup, login } = require("../models/users.model");
+
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  if (validate(username, password)) {
+    return signup(res, username, password);
+  }
+  return res.send({
+    success: false,
+    data: null,
+    error: "Invalid data provided by user",
+  });
+});
+
+function validate(username, password) {
+  return (
+    username &&
+    username.length >= 4 &&
+    username.length <= 20 &&
+    password &&
+    password.length >= 4 &&
+    password.length <= 20
+  );
+}
 module.exports = router;
