8251bbe935fa1f95c48c5c57f9a0694307311b7e
Michael Zimmerman <mikezne@gmail.com>
2021-07-17 14:35:42 -0500

    wires up the login page to make an api call

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,13 @@
 import React, { useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import useFetch from "../hooks/useFetch";
 
 const Login = () => {
   const { login } = useContext(UserContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const { callAPI: loginCall } = useFetch("POST");
+  const [error, setError] = useState(null);
   return (
     <>
       <h2>Login</h2>
@@ -29,16 +32,30 @@ const Login = () => {
 
         <button
           className=""
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault();
-            if (username.length > 3 && password.length > 3) {
-              login(username);
+            if (
+              username.length > 4 &&
+              password.length > 4 &&
+              username.length <= 20 &&
+              password.length <= 20
+            ) {
+              setError(null);
+              let res = await loginCall("/api/users/login", {
+                username,
+                password,
+              });
+              if (res.error) {
+                return setError(res.error);
+              }
+              login(res.data.username, res.data.id);
             }
           }}
         >
           Login
         </button>
       </form>
+      <div>{error}</div>
     </>
   );
 };
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -4,10 +4,17 @@ export const UserContext = createContext("");
 
 export function UserProvider(props) {
   const [username, setUsername] = useState(null);
-  const login = useCallback((userN) => setUsername(userN), []);
-  const logout = useCallback(() => setUsername(null), []);
+  const [user_id, setUser_id] = useState(null);
+  const login = useCallback((userN, id) => {
+    setUsername(userN);
+    setUser_id(id);
+  }, []);
+  const logout = useCallback(() => {
+    setUsername(null);
+    setUser_id(null);
+  }, []);
   return (
-    <UserContext.Provider value={{ username, login, logout }}>
+    <UserContext.Provider value={{ username, user_id, login, logout }}>
       {props.children}
     </UserContext.Provider>
   );
