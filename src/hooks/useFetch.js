fa118665cfa92d76d48a040056717a482dce603d
Michael Zimmerman <mikezne@gmail.com>
2021-07-17 14:23:38 -0500

    adds useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+export default function useFetch(method) {
+  async function callAPI(url, body = null) {
+    let opts = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (method === "POST") {
+      opts = { ...opts, body: JSON.stringify(body) };
+    }
+    try {
+      const response = await fetch(url, opts);
+      if (response.ok) {
+        const json = await response.json();
+        return json;
+      } else {
+        throw response;
+      }
+    } catch (e) {
+      console.log(e);
+      return { error: "Something went wrong, please try again later" };
+    }
+  }
+  // Exposes the data, any error, and whether or not it was loading
+  return { callAPI };
+}
