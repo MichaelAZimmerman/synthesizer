b559ad5fc1ac358b61008d806b854abef5fc95eb
Michael Zimmerman <mikezne@gmail.com>
2021-09-14 16:59:46 -0500

    changes last updated date

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ function App() {
           </main>
 
           <footer className="text-center">
-            <div>Last updated: 9/11/2021</div>
+            <div>Last updated: 9/14/2021</div>
           </footer>
         </>
       ) : (
