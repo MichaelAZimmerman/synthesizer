d79483b346267874698c82e6b0fd064afd64551a
Michael Zimmerman <mikezne@gmail.com>
2021-09-16 23:24:29 -0500

    changes some keyboard styling

--- src/context/SynthContext.js
+++ src/context/SynthContext.js
@@ -3,8 +3,8 @@ import React, { useState, createContext } from "react";
 export const SynthContext = createContext(null);
 
 export function SynthProvider(props) {
-  const [octave, setOctave] = useState(3);
-  const [octaveTwo, setOctaveTwo] = useState(4);
+  const [octave, setOctave] = useState(4);
+  const [octaveTwo, setOctaveTwo] = useState(5);
   const [noteType, setNoteType] = useState("16n");
   const [oscType, setOscType] = useState("triangle");
   const [oscTypeTwo, setOscTypeTwo] = useState("sine");
